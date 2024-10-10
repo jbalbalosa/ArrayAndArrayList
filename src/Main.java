2aadf9eab759640191b85fa683cb8f1fdade65d0
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2024-10-10 10:20:51 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Array And ArrayList!");
+
+        String[] originalArray = new String[] {"First", "Second", "Third"};
+        var originalList = Arrays.asList(originalArray);
+
+        originalList.set(0,"one");
+        System.out.println("list: " + originalList);
+        System.out.println("array: " + Arrays.toString(originalArray));
+
+        originalList.sort(Comparator.naturalOrder());
+        System.out.println("array: " + Arrays.toString(originalArray));
+
+        List<String> newlist = Arrays.asList("Sunday","Monday","Tuesday");
+        System.out.println(newlist);
     }
 }
